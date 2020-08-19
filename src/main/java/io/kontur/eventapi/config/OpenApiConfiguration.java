cfa8a8951032cc57412a8ab467020197c3f7112a
Alexander Zapasnik <sanych27@gmail.com>
2020-08-19 16:22:46 +0300

    Fix swagger ui https requests

--- src/main/java/io/kontur/eventapi/config/OpenApiConfiguration.java
+++ src/main/java/io/kontur/eventapi/config/OpenApiConfiguration.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration
 public class OpenApiConfiguration {
@@ -14,9 +17,12 @@ public class OpenApiConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
         String securitySchemeName = "bearerAuth";
+        Server server = new Server();
+        server.setUrl("/events");
         return new OpenAPI().info(new Info()
                 .title("Event API")
                 .description("Disaster footprints & alerts"))
+                .servers(Collections.singletonList(server))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
