340d703f4f0f7a8c46392431b840f85682a0f5e8
Alexander Zapasnik <sanych27@gmail.com>
2020-08-25 09:10:28 +0300

    US243 Ingest PDS hazards from SQS

--- src/test/java/io/kontur/eventapi/EventapiApplicationTests.java
+++ src/test/java/io/kontur/eventapi/EventapiApplicationTests.java
@@ -1,6 +1,5 @@
 package io.kontur.eventapi;
 
-import io.kontur.eventapi.test.TestConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
