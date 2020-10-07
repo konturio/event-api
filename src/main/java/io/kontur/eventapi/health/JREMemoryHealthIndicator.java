1f5226f3f73d57444fcca0268d689e7359e511bd
Alexander Zapasnik <sanych27@gmail.com>
2020-10-07 18:17:52 +0300

    US153 Add prometheus metrics endpoint

--- src/main/java/io/kontur/eventapi/health/JREMemoryHealthIndicator.java
+++ src/main/java/io/kontur/eventapi/health/JREMemoryHealthIndicator.java
@@ -4,6 +4,9 @@ import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.stereotype.Component;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+
 @Component
 public class JREMemoryHealthIndicator extends AbstractHealthIndicator {
 
@@ -11,10 +14,15 @@ public class JREMemoryHealthIndicator extends AbstractHealthIndicator {
     protected void doHealthCheck(Health.Builder builder) {
         builder.up();
 
-        final long totalMemory = Runtime.getRuntime().totalMemory();
-        final long freeMemory = Runtime.getRuntime().freeMemory();
-        builder.withDetail("usedMemory", totalMemory - freeMemory)
-                .withDetail("maxAvailableMemory", totalMemory);
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        long usedMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
+        long maxMemory = memoryMXBean.getHeapMemoryUsage().getMax();
+        long usedNonHeap = memoryMXBean.getNonHeapMemoryUsage().getUsed();
+        long maxNonHeap = memoryMXBean.getNonHeapMemoryUsage().getMax();
+        builder.withDetail("heap.usedMemory", usedMemory)
+                .withDetail("heap.maxAvailableMemory", maxMemory)
+                .withDetail("nonHeap.usedMemory", usedNonHeap)
+                .withDetail("nonHeap.maxAvailableMemory", maxNonHeap == -1 ? "undefined" : maxNonHeap);
     }
 
 }
