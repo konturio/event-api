16bb68236643c85952adbb8fe8556cadc97c459f
Aliaksei Kavaliou <lyakseihc93@gmail.com>
2020-10-30 00:10:26 +0300

    3571 add test combination event

--- src/test/java/io/kontur/eventapi/job/FeedCompositionJobIT.java
+++ src/test/java/io/kontur/eventapi/job/FeedCompositionJobIT.java
@@ -1,5 +1,6 @@
 package io.kontur.eventapi.job;
 
+import io.kontur.eventapi.dao.KonturEventsDao;
 import io.kontur.eventapi.dao.mapper.DataLakeMapper;
 import io.kontur.eventapi.dao.mapper.FeedMapper;
 import io.kontur.eventapi.entity.DataLake;
@@ -21,6 +22,7 @@ import java.util.UUID;
 import static io.kontur.eventapi.pdc.converter.PdcDataLakeConverter.HP_SRV_MAG_PROVIDER;
 import static io.kontur.eventapi.pdc.converter.PdcDataLakeConverter.HP_SRV_SEARCH_PROVIDER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FeedCompositionJobIT extends AbstractIntegrationTest {
 
@@ -29,16 +31,18 @@ public class FeedCompositionJobIT extends AbstractIntegrationTest {
     private final FeedCompositionJob feedCompositionJob;
     private final DataLakeMapper dataLakeMapper;
     private final FeedMapper feedMapper;
+    private final KonturEventsDao konturEventsDao;
 
     @Autowired
     public FeedCompositionJobIT(NormalizationJob normalizationJob, EventCombinationJob eventCombinationJob,
                                 FeedCompositionJob feedCompositionJob, DataLakeMapper dataLakeMapper,
-                                FeedMapper feedMapper) {
+                                FeedMapper feedMapper, KonturEventsDao konturEventsDao) {
         this.normalizationJob = normalizationJob;
         this.eventCombinationJob = eventCombinationJob;
         this.feedCompositionJob = feedCompositionJob;
         this.dataLakeMapper = dataLakeMapper;
         this.feedMapper = feedMapper;
+        this.konturEventsDao = konturEventsDao;
     }
 
     @Test
@@ -78,15 +82,7 @@ public class FeedCompositionJobIT extends AbstractIntegrationTest {
     }
 
     private FeedData createFeed(String externalEventUUId, OffsetDateTime loadedTime, String provider, String data) {
-        var dataLake = new DataLake();
-        dataLake.setObservationId(UUID.randomUUID());
-        dataLake.setExternalId(externalEventUUId);
-        dataLake.setLoadedAt(loadedTime);
-        dataLake.setProvider(provider);
-        dataLake.setData(data);
-
-        dataLakeMapper.create(dataLake);
-        normalizationJob.run();
+        createNormalizations(externalEventUUId, loadedTime, provider, data);
         eventCombinationJob.run();
         feedCompositionJob.run();
 
@@ -98,9 +94,45 @@ public class FeedCompositionJobIT extends AbstractIntegrationTest {
         ).get(0);
     }
 
+    private void createNormalizations(String externalEventUUId, OffsetDateTime loadedTime, String provider, String data){
+        var dataLake = new DataLake();
+        dataLake.setObservationId(UUID.randomUUID());
+        dataLake.setExternalId(externalEventUUId);
+        dataLake.setLoadedAt(loadedTime);
+        dataLake.setProvider(provider);
+        dataLake.setData(data);
+
+        dataLakeMapper.create(dataLake);
+        normalizationJob.run();
+    }
+
     @Test
-    public void testSavedMags(){
-        String eventUUID = "0457178b-45c1-492f-bbc1-61ca14389a31";
+    public void testSavedMags() throws IOException {
+        String externalId = "996d6b0a-ce18-47d9-9bd2-b1b8fe5d967a";
+        var hazardsLoadTime = OffsetDateTime.of(
+                LocalDateTime.of(2020, 10, 1, 1, 1, 1), ZoneOffset.UTC);
+        var mags01LoadTime = OffsetDateTime.of(
+                LocalDateTime.of(2020, 10, 1, 1, 1, 2), ZoneOffset.UTC);
+        var mags02LoadTime = OffsetDateTime.of(
+                LocalDateTime.of(2020, 10, 1, 1, 3), ZoneOffset.UTC);
+
+        createNormalizations(externalId, hazardsLoadTime, HP_SRV_SEARCH_PROVIDER, readMessageFromFile("hpsrvhazard02.json"));
+        var eventOptionalVersion1 = konturEventsDao.getLatestEventByExternalId(externalId);
+        assertTrue(eventOptionalVersion1.isPresent());
+        assertEquals(1, eventOptionalVersion1.get().getObservationIds().size());
+
+        createNormalizations(externalId, mags01LoadTime, HP_SRV_MAG_PROVIDER, readMessageFromFile("magsdata02.json"));
+        eventCombinationJob.run();
+        createNormalizations(externalId, mags02LoadTime, HP_SRV_MAG_PROVIDER, readMessageFromFile("magsdata03.json"));
+        eventCombinationJob.run();
+        var eventOptionalVersion2 = konturEventsDao.getLatestEventByExternalId(externalId);
+        assertTrue(eventOptionalVersion2.isPresent());
+        assertEquals(2, eventOptionalVersion2.get().getObservationIds().size());
+
+        eventCombinationJob.run();
+        var eventOptionalVersion3 = konturEventsDao.getLatestEventByExternalId(externalId);
+        assertTrue(eventOptionalVersion3.isPresent());
+        assertEquals(3, eventOptionalVersion3.get().getObservationIds().size());
     }
 
     private String readMessageFromFile(String fileName) throws IOException {
