aed557dd680a3a2e96000b35a40862eefc3f9446
Alexander Zapasnik <sanych27@gmail.com>
2021-01-23 09:15:06 +0300

    #4652 Rename em-dat feed.

--- src/main/resources/db/changelog/v0/add-em-dat-feed.sql
+++ src/main/resources/db/changelog/v0/add-em-dat-feed.sql
@@ -3,5 +3,5 @@
 --changeset event-api-migrations:v0/add-em-dat-feed.sql runOnChange:false
 
 INSERT INTO feeds (feed_id, alias, description, providers)
-VALUES (uuid_generate_v4(), 'emdat', 'EM-DAT', '{"em-dat"}');
+VALUES (uuid_generate_v4(), 'em-dat', 'EM-DAT', '{"em-dat"}');
 
