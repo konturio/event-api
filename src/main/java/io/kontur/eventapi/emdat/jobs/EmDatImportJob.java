d71a072c77ea6dfc5860b5b9613a597b689c41d9
Alexander Zapasnik <sanych27@gmail.com>
2021-02-01 14:33:54 +0300

    #4808 Use fastexcel-reader for xlsx parsing

--- src/main/java/io/kontur/eventapi/emdat/jobs/EmDatImportJob.java
+++ src/main/java/io/kontur/eventapi/emdat/jobs/EmDatImportJob.java
@@ -8,7 +8,10 @@ import io.kontur.eventapi.util.DateTimeUtil;
 import io.micrometer.core.annotation.Counted;
 import io.micrometer.core.annotation.Timed;
 import org.apache.commons.text.StringEscapeUtils;
-import org.apache.poi.ss.usermodel.*;
+import org.dhatim.fastexcel.reader.Cell;
+import org.dhatim.fastexcel.reader.ReadableWorkbook;
+import org.dhatim.fastexcel.reader.Row;
+import org.dhatim.fastexcel.reader.Sheet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -31,7 +34,6 @@ public class EmDatImportJob implements Runnable {
     private static final Logger LOG = LoggerFactory.getLogger(EmDatImportJob.class);
     private static final String CSV_ID_HEADER = "Dis No";
     private static final String FILE_CREATION_CELL = "File creation:";
-    private static final DataFormatter DATA_FORMATTER = new DataFormatter(Locale.ENGLISH);
     private static final DateTimeFormatter FILE_CREATION_FORMATTER = DateTimeFormatter
             .ofPattern("EEE, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);
     private final EmDatImportService importService;
@@ -51,7 +53,7 @@ public class EmDatImportJob implements Runnable {
         EmDatPublicFile emDatPublicFile = importService.obtainFileStatistic(token);
 
         try (final InputStream stream = importService.obtainFile(emDatPublicFile.getName(), token);
-             final Workbook wb = WorkbookFactory.create(stream)) {
+             final ReadableWorkbook wb = new ReadableWorkbook(stream)) {
             Sheet sheet = getDataSheet(wb);
             parseAndSaveContent(sheet);
         } catch (IOException e) {
@@ -60,20 +62,24 @@ public class EmDatImportJob implements Runnable {
         LOG.info("EM-DAT import job has finished");
     }
 
-    private Sheet getDataSheet(Workbook wb) {
-        Sheet sheet = wb.getSheet("emdat data");
-        if (sheet == null) {
+    private Sheet getDataSheet(ReadableWorkbook wb) {
+        Optional<Sheet> sheet = wb.getSheets()
+                .filter(s -> "emdat data".equals(s.getName()))
+                .findFirst();
+
+        if (sheet.isEmpty()) {
             throw new IllegalArgumentException("Illegal EM-DAT xlsx format. Could not find 'emdat data' sheet");
         }
-        return sheet;
+        return sheet.get();
     }
 
-    private void parseAndSaveContent(Sheet sheet) {
-        OffsetDateTime fileCreationDate = getFileCreationDate(sheet);
-        int headerRowNum = findHeaderRow(sheet);
-        String csvHeader = convertRowIntoCsv(sheet.getRow(headerRowNum));
-        for (int dataRowNum = headerRowNum + 1; dataRowNum <= sheet.getLastRowNum(); dataRowNum++) {
-            String csvData = convertRowIntoCsv(sheet.getRow(dataRowNum));
+    private void parseAndSaveContent(Sheet sheet) throws IOException {
+        List<Row> rows = sheet.read();
+        OffsetDateTime fileCreationDate = getFileCreationDate(rows);
+        int headerRowNum = findHeaderRow(rows);
+        String csvHeader = convertRowIntoCsv(rows.get(headerRowNum));
+        for (int dataRowNum = headerRowNum + 1; dataRowNum < rows.size(); dataRowNum++) {
+            String csvData = convertRowIntoCsv(rows.get(dataRowNum));
             try {
                 Map<String, String> row = parseRow(csvHeader, csvData);
                 String externalId = row.get(CSV_ID_HEADER);
@@ -94,20 +100,23 @@ public class EmDatImportJob implements Runnable {
         return dataLake.isEmpty() || !data.equals(dataLake.get().getData());
     }
 
-    private int findHeaderRow(Sheet sheet) {
-        for (int i = 0; i < sheet.getLastRowNum(); i++) {
-            if (CSV_ID_HEADER.equals(DATA_FORMATTER.formatCellValue(sheet.getRow(i).getCell(0)))) {
+    private int findHeaderRow(List<Row> rows) {
+        for (int i = 0; i < rows.size(); i++) {
+            Optional<String> cellValue = rows.get(i).getCellAsString(0);
+            if (cellValue.isPresent() && CSV_ID_HEADER.equals(cellValue.get())) {
                 return i;
             }
         }
         throw new IllegalArgumentException("Illegal EM-DAT xlsx format. Could not find table header");
     }
 
-    private OffsetDateTime getFileCreationDate(Sheet sheet) {
-        for (Row row : sheet) {
-            if (FILE_CREATION_CELL.equals(DATA_FORMATTER.formatCellValue(row.getCell(0)))) {
+    private OffsetDateTime getFileCreationDate(List<Row> rows) {
+        for (Row row : rows) {
+            Cell cellName = row.getCell(0);
+            if (cellName != null && FILE_CREATION_CELL.equals(cellName.getText())) {
+                String cellValue = row.getCell(1).getText();
                 return ZonedDateTime
-                        .parse(DATA_FORMATTER.formatCellValue(row.getCell(1)), FILE_CREATION_FORMATTER)
+                        .parse(cellValue, FILE_CREATION_FORMATTER)
                         .toOffsetDateTime();
             }
         }
@@ -117,13 +126,15 @@ public class EmDatImportJob implements Runnable {
 
     private String convertRowIntoCsv(Row row) {
         StringBuilder builder = new StringBuilder();
-        for (int cn = 0; cn < row.getLastCellNum(); cn++) {
-            Cell cell = row.getCell(cn, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
-            String cellValue = DATA_FORMATTER.formatCellValue(cell);
+        for (int cn = 0; cn < row.getCellCount(); cn++) {
+            String cellValue = null;
+            if (row.getCell(cn) != null) {
+                cellValue = row.getCell(cn).getText();
+            }
             if (cellValue != null && !cellValue.isBlank()) {
                 builder.append(StringEscapeUtils.escapeCsv(cellValue));
             }
-            if (cn < row.getLastCellNum() - 1) {
+            if (cn < row.getCellCount() - 1) {
                 builder.append(",");
             }
         }
