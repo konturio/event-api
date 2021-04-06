6fd0b0d9ef79b6756224e42b89f3c4dbde0df262
Polina <pkrukovich@kontur.io>
2021-04-06 19:23:48 +0300

    #5258 split files from wildfire.frap.cal
    Former-commit-id: 839ff14854c79889bc4210de1994ff93e5f1e547

--- src/main/java/io/kontur/eventapi/staticdata/config/StaticDataConfig.java
+++ src/main/java/io/kontur/eventapi/staticdata/config/StaticDataConfig.java
@@ -4,13 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class StaticDataConfig {
     private final static String STATIC_FILES_FOLDER = "static/";
 
     @Bean
-    public StaticFileData getTornadoCanadaGovFileData() {
+    public List<StaticFileData> getStaticFiles() {
+        List<StaticFileData> staticFiles = new ArrayList<>();
+        staticFiles.add(getTornadoCanadaGovFileData());
+        staticFiles.add(getTornadoAustralianBmFileData());
+        staticFiles.add(getTornadoOsmFileData());
+        staticFiles.add(getTornadoDesInventarSendaiFileData());
+        staticFiles.addAll(getWildfireFrapCalFilesData());
+
+        return staticFiles;
+    }
+
+    private StaticFileData getTornadoCanadaGovFileData() {
         return new StaticFileData (
                 STATIC_FILES_FOLDER + "tornado-canada-gov.geojson",
                 "tornado.canada-gov",
@@ -18,8 +32,7 @@ public class StaticDataConfig {
                 "geojson");
     }
 
-    @Bean
-    public StaticFileData getTornadoAustralianBmFileData() {
+    private StaticFileData getTornadoAustralianBmFileData() {
         return new StaticFileData (
                 STATIC_FILES_FOLDER + "tornado-australian-bm.geojson",
                 "tornado.australian-bm",
@@ -27,8 +40,7 @@ public class StaticDataConfig {
                 "geojson");
     }
 
-    @Bean
-    public StaticFileData getTornadoOsmFileData() {
+    private StaticFileData getTornadoOsmFileData() {
         return new StaticFileData (
                 STATIC_FILES_FOLDER + "tornado-osm-wiki.geojson",
                 "tornado.osm-wiki",
@@ -36,8 +48,7 @@ public class StaticDataConfig {
                 "geojson");
     }
 
-    @Bean
-    public StaticFileData getTornadoDesInventarSendaiFileData() {
+    private StaticFileData getTornadoDesInventarSendaiFileData() {
         return new StaticFileData (
                 STATIC_FILES_FOLDER + "tornado-des-inventar-sendai.geojson",
                 "tornado.des-inventar-sendai",
@@ -45,13 +56,14 @@ public class StaticDataConfig {
                 "geojson");
     }
 
-    @Bean
-    public StaticFileData getWildfireFrapCalFileData() {
-        return new StaticFileData (
-                STATIC_FILES_FOLDER + "wildfire-frap-cal.geojson",
-                "wildfire.frap.cal",
-                OffsetDateTime.parse("2020-05-01T00:00:00Z"),
-                "geojson"
-        );
+    private List<StaticFileData> getWildfireFrapCalFilesData() {
+        return List.of("1870-1930", "1930-1960", "1960-1980", "1980-2000", "2000-2010", "2010-2015", "2015-2020")
+                .stream()
+                .map(year -> new StaticFileData(
+                        String.format("%swildfire-frap-cal_%s.geojson", STATIC_FILES_FOLDER, year),
+                        "wildfire.frap.cal",
+                        OffsetDateTime.parse("2020-05-01T00:00:00Z"),
+                        "geojson"))
+                .collect(Collectors.toList());
     }
 }
