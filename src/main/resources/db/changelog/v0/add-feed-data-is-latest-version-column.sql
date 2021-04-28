a810c6de0de99bc83f8f5aa6f9bbd483d167f6fe
Alexander Zapasnik <sanych27@gmail.com>
2021-04-28 10:01:51 +0300

    #4736 Update add-feed-data-is-latest-version-column.sql script.
    Former-commit-id: 8d7b88eec8da33c199485140f1d2766cc1932be5

--- src/main/resources/db/changelog/v0/add-feed-data-is-latest-version-column.sql
+++ src/main/resources/db/changelog/v0/add-feed-data-is-latest-version-column.sql
@@ -1,19 +1,24 @@
 --liquibase formatted sql
 
 --changeset event-api-migrations:v0/add-feed-data-is-latest-version-column.sql runOnChange:false
+--preconditions onFail:MARK_RAN
+--precondition-sql-check expectedResult:0 SELECT count(*) FROM information_schema.columns WHERE table_name = 'feed_data' AND column_name = 'is_latest_version';
 
-alter table feed_data add is_latest_version bool default false;
+alter table feed_data
+    add is_latest_version bool default false;
 
-update feed_data fd set is_latest_version = true where not exists (
-        select
-        from
-            feed_data prev
-        where
-                fd.feed_id = prev.feed_id
-          and fd.event_id = prev.event_id
-          and fd.version < prev.version);
-
+with events as (
+    select feed_id, event_id, max(version) as "version"
+    from feed_data
+    group by feed_id, event_id
+)
+update feed_data fd
+set is_latest_version = true
+from events e
+where fd.feed_id = e.feed_id
+  and fd.event_id = e.event_id
+  and fd.version = e.version;
 
 drop index if exists feed_data_updated_at_idx;
 
-CREATE INDEX feed_data_updated_at_feed_id_is_latest_version_idx ON public.feed_data USING btree (updated_at, feed_id, is_latest_version);
+CREATE INDEX if not exists feed_data_updated_at_feed_id_is_latest_version_idx ON public.feed_data USING btree (updated_at, feed_id, is_latest_version);
