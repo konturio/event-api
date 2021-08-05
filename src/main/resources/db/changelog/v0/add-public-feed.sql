8f441b486d4d9d6147422a002ad3ee1d481c6ec6
Polina Krukovich <pkrukovich@kontur.io>
2021-08-05 09:42:15 +0000

    US759 Add new feed (for public access)
    Former-commit-id: f7f464ef9bf19edb6623b10a869e7c97c38458e5

---
+++ src/main/resources/db/changelog/v0/add-public-feed.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset event-api-migrations:v0/add-public-feed.sql runOnChange:false
+
+INSERT INTO feeds (feed_id, alias, description, providers)
+VALUES (uuid_generate_v4(), 'kontur-public', 'Public feed','{"gdacsAlert","gdacsAlertGeometry"}');
+
+insert into feed_event_status (feed_id, event_id, actual)
+select distinct on (event_id) fs.feed_id, event_id, false
+from kontur_events, feeds fs
+where provider in ('gdacsAlert', 'gdacsAlertGeometry') and alias = 'kontur-public'
+on conflict do nothing;
