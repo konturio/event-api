a90888e22d71f61dd56cdf8a5b908a677cfb2963
Polina Krukovich <pkrukovich@kontur.io>
2021-08-19 15:13:09 +0300

    #6858 add enrichment to gdacs feed
    Former-commit-id: 07e4a1809842b761fe6ef66c69ffdb901af7827c

--- src/main/resources/db/changelog/v0/feed-analytics.sql
+++ src/main/resources/db/changelog/v0/feed-analytics.sql
@@ -3,3 +3,4 @@
 --changeset event-api-migrations:feed-analytics.sql runOnChange:true
 
 update feeds set enrichment = '{"population", "peopleWithoutOsmBuildings", "areaWithoutOsmBuildingsKm2", "peopleWithoutOsmRoads", "areaWithoutOsmRoadsKm2", "peopleWithoutOsmObjects", "areaWithoutOsmObjectsKm2", "osmGapsPercentage", "industrialAreaKm2", "forestAreaKm2", "volcanoesCount", "hotspotDaysPerYearMax"}' where alias = 'gdacs-firms';
+update feeds set enrichment = '{"population", "peopleWithoutOsmBuildings", "areaWithoutOsmBuildingsKm2", "peopleWithoutOsmRoads", "areaWithoutOsmRoadsKm2", "peopleWithoutOsmObjects", "areaWithoutOsmObjectsKm2", "osmGapsPercentage"}' where alias = 'gdacs';
