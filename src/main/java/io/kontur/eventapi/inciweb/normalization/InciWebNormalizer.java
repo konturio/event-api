7cab9df825669b16663183196508eabd7631e1db
Konstantin Shmidman <kshmidman@kontur.io>
2021-12-08 10:28:43 +0000

    7857-inciweb-data-normalization
    Former-commit-id: d224b649e060967aac997154a9c82210e66dc016

---
+++ src/main/java/io/kontur/eventapi/inciweb/normalization/InciWebNormalizer.java
@@ -0,0 +1,68 @@
+package io.kontur.eventapi.inciweb.normalization;
+
+import static io.kontur.eventapi.inciweb.converter.InciWebDataLakeConverter.INCIWEB_PROVIDER;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+import io.kontur.eventapi.entity.DataLake;
+import io.kontur.eventapi.entity.EventType;
+import io.kontur.eventapi.entity.NormalizedObservation;
+import io.kontur.eventapi.entity.Severity;
+import io.kontur.eventapi.inciweb.converter.InciWebXmlParser;
+import io.kontur.eventapi.inciweb.dto.ParsedItem;
+import io.kontur.eventapi.normalization.Normalizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.wololo.geojson.Feature;
+import org.wololo.geojson.FeatureCollection;
+import org.wololo.geojson.Geometry;
+import org.wololo.geojson.Point;
+
+@Component
+public class InciWebNormalizer extends Normalizer {
+
+    private final static Logger LOG = LoggerFactory.getLogger(InciWebNormalizer.class);
+
+    @Override
+    public boolean isApplicable(DataLake dataLakeDto) {
+        return INCIWEB_PROVIDER.equals(dataLakeDto.getProvider());
+    }
+
+    @Override
+    public NormalizedObservation normalize(DataLake dataLakeDto) {
+        Optional<ParsedItem> parsedItem = new InciWebXmlParser().getParsedItem(dataLakeDto.getData());
+        if (parsedItem.isPresent()) {
+            NormalizedObservation normalizedObservation = new NormalizedObservation();
+
+            normalizedObservation.setObservationId(dataLakeDto.getObservationId());
+            normalizedObservation.setExternalEventId(dataLakeDto.getExternalId());
+            normalizedObservation.setProvider(dataLakeDto.getProvider());
+            normalizedObservation.setEventSeverity(Severity.UNKNOWN);
+            normalizedObservation.setName(parsedItem.get().getTitle());
+            normalizedObservation.setDescription(parsedItem.get().getDescription());
+            normalizedObservation.setType(EventType.WILDFIRE);
+            normalizedObservation.setLoadedAt(dataLakeDto.getLoadedAt());
+            normalizedObservation.setStartedAt(dataLakeDto.getUpdatedAt());
+            normalizedObservation.setEndedAt(dataLakeDto.getUpdatedAt());
+            normalizedObservation.setSourceUpdatedAt(dataLakeDto.getUpdatedAt());
+            normalizedObservation.setSourceUri(parsedItem.get().getLink());
+
+            Point point = new Point(new double[] {parsedItem.get().getLongitude(), parsedItem.get().getLatitude()});
+            normalizedObservation.setGeometries(convertGeometries(point));
+            normalizedObservation.setPoint(
+                    makeWktPoint(parsedItem.get().getLongitude(), parsedItem.get().getLatitude()));
+
+            return normalizedObservation;
+        } else {
+            LOG.error("Can't parse input source for normalization from xml: {}", dataLakeDto.getData());
+        }
+        return null;
+    }
+
+    private FeatureCollection convertGeometries(Geometry geometry) {
+        return new FeatureCollection(new Feature[]{new Feature(geometry, new HashMap<>())});
+    }
+
+}
---
+++ src/test/java/io/kontur/eventapi/inciweb/normalization/InciWebNormalizationTest.java
@@ -0,0 +1,75 @@
+package io.kontur.eventapi.inciweb.normalization;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import io.kontur.eventapi.entity.DataLake;
+import io.kontur.eventapi.entity.EventType;
+import io.kontur.eventapi.entity.NormalizedObservation;
+import io.kontur.eventapi.entity.Severity;
+import io.kontur.eventapi.inciweb.converter.InciWebDataLakeConverter;
+import io.kontur.eventapi.inciweb.converter.InciWebXmlParser;
+import io.kontur.eventapi.inciweb.dto.ParsedItem;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.wololo.geojson.Feature;
+import org.wololo.geojson.FeatureCollection;
+import org.wololo.geojson.Point;
+
+class InciWebNormalizationTest {
+
+    @Test
+    public void testIsApplicable() throws Exception {
+        DataLake dataLake = createDataLake();
+        assertTrue(new InciWebNormalizer().isApplicable(dataLake));
+    }
+
+    @Test
+    public void testNormalization() throws Exception {
+        //given
+        DataLake dataLake = createDataLake();
+
+        //when
+        NormalizedObservation observation = new InciWebNormalizer().normalize(dataLake);
+
+        //then
+        assertEquals(dataLake.getObservationId(), observation.getObservationId());
+        assertEquals(InciWebDataLakeConverter.INCIWEB_PROVIDER, observation.getProvider());
+        assertEquals("http://example.com/incident/1/", observation.getExternalEventId());
+        assertNull(observation.getExternalEpisodeId());
+        assertEquals(Severity.UNKNOWN, observation.getEventSeverity());
+        assertEquals("Title 1", observation.getName());
+        assertEquals("Description 1", observation.getDescription());
+        assertEquals(EventType.WILDFIRE, observation.getType());
+        assertEquals(dataLake.getUpdatedAt(), observation.getStartedAt());
+        assertEquals(dataLake.getUpdatedAt(), observation.getEndedAt());
+        assertEquals(dataLake.getUpdatedAt(), observation.getSourceUpdatedAt());
+        assertEquals(dataLake.getLoadedAt(), observation.getLoadedAt());
+        assertEquals("http://example.com/incident/1/", observation.getSourceUri());
+        assertEquals("POINT(-110.111 10.111)", observation.getPoint());
+        checkGeometriesValue(observation.getGeometries());
+    }
+
+    private void checkGeometriesValue(FeatureCollection geom) {
+        assertNotNull(geom);
+        assertEquals(1, geom.getFeatures().length);
+        Feature feature = geom.getFeatures()[0];
+        assertTrue(feature.getGeometry() instanceof Point);
+        Point point = (Point) feature.getGeometry();
+        assertEquals(-110.111, point.getCoordinates()[0]);
+        assertEquals(10.111, point.getCoordinates()[1]);
+    }
+
+    private DataLake createDataLake() throws Exception {
+        String data = IOUtils.toString(
+                Objects.requireNonNull(this.getClass().getResourceAsStream("data.xml")), "UTF-8");
+        Optional<ParsedItem> parsedItem = new InciWebXmlParser().getParsedItem(data);
+        assertTrue(parsedItem.isPresent());
+        return new InciWebDataLakeConverter().convertEvent(parsedItem.get());
+    }
+}
