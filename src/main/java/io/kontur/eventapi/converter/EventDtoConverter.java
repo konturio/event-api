d953ada62e7535c80a00d7c225a7b79f476b8f04
Polina Krukovich <pkrukovich@kontur.io>
2022-04-20 13:25:13 +0000

    9992 fix 500 error for geojson endpoint
    Former-commit-id: 55466aabe0ee7079eaf71e2d3b881149228c4e49

--- src/main/java/io/kontur/eventapi/converter/EventDtoConverter.java
+++ src/main/java/io/kontur/eventapi/converter/EventDtoConverter.java
@@ -8,6 +8,9 @@ import org.wololo.geojson.Feature;
 
 import java.util.*;
 
+import static java.util.Collections.emptyMap;
+import static java.util.stream.Collectors.toList;
+
 public class EventDtoConverter {
 
     public static EventDto convert(FeedData dataDto) {
@@ -19,32 +22,38 @@ public class EventDtoConverter {
     public static List<Feature> convertGeoJson(FeedData event) {
         List<Feature> features = new ArrayList<>();
         for (FeedEpisode episode : event.getEpisodes()) {
-            for (Feature episodeFeature : episode.getGeometries().getFeatures()) {
-                Map<String, Object> properties = new HashMap<>();
-                for (var episodeFeatureProperty : episodeFeature.getProperties().entrySet()) {
-                    properties.put("feature_" + episodeFeatureProperty.getKey(), episodeFeatureProperty.getValue());
-                }
-                properties.put("eventId", event.getEventId());
-                properties.put("episode_name", episode.getName());
-                properties.put("episode_properName", episode.getProperName());
-                properties.put("episode_description", episode.getDescription());
-                properties.put("episode_type", episode.getType());
-                properties.put("episode_active", episode.getActive());
-                properties.put("episode_severity", episode.getSeverity());
-                properties.put("episode_startedA", episode.getStartedAt());
-                properties.put("episode_endedAt", episode.getEndedAt());
-                properties.put("episode_updatedAt", episode.getUpdatedAt());
-                properties.put("episode_sourceUpdatedAt", episode.getSourceUpdatedAt());
-                properties.put("episode_observations", episode.getObservations());
-                properties.put("episode_episodeDetails", episode.getEpisodeDetails());
-                properties.put("episode_urls", episode.getUrls());
-                properties.put("episode_location", episode.getLocation());
-
-                Feature feature = new Feature(episodeFeature.getGeometry(), properties);
-                features.add(feature);
+            if (episode.getGeometries() != null && episode.getGeometries().getFeatures() != null) {
+                features.addAll(Arrays.stream(episode.getGeometries().getFeatures())
+                        .map(episodeFeature -> createFeature(event, episode, episodeFeature)).toList());
+            } else {
+                features.add(createFeature(event, episode, new Feature(null, emptyMap())));
             }
         }
         return features;
+    }
+
+    private static Feature createFeature(FeedData event, FeedEpisode episode, Feature episodeFeature) {
+        Map<String, Object> properties = new HashMap<>();
+        for (var episodeFeatureProperty : episodeFeature.getProperties().entrySet()) {
+            properties.put("feature_" + episodeFeatureProperty.getKey(), episodeFeatureProperty.getValue());
+        }
+        properties.put("eventId", event.getEventId());
+        properties.put("episode_name", episode.getName());
+        properties.put("episode_properName", episode.getProperName());
+        properties.put("episode_description", episode.getDescription());
+        properties.put("episode_type", episode.getType());
+        properties.put("episode_active", episode.getActive());
+        properties.put("episode_severity", episode.getSeverity());
+        properties.put("episode_startedA", episode.getStartedAt());
+        properties.put("episode_endedAt", episode.getEndedAt());
+        properties.put("episode_updatedAt", episode.getUpdatedAt());
+        properties.put("episode_sourceUpdatedAt", episode.getSourceUpdatedAt());
+        properties.put("episode_observations", episode.getObservations());
+        properties.put("episode_episodeDetails", episode.getEpisodeDetails());
+        properties.put("episode_urls", episode.getUrls());
+        properties.put("episode_location", episode.getLocation());
+
+        return new Feature(episodeFeature.getGeometry(), properties);
     }
 
 }
