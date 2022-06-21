89e9afd46dc8d9fbfa6052612c26ca532e6c88f0
kshmidman <kshmidman@gmail.com>
2022-06-21 15:51:35 +0300

    #10761. nifc provider error duplicate key value violates unique constraint
    Former-commit-id: 7a67046dffd5ea28d989cb02a61d3f3924aa6720

--- src/main/java/io/kontur/eventapi/nifc/job/NifcImportJob.java
+++ src/main/java/io/kontur/eventapi/nifc/job/NifcImportJob.java
@@ -6,13 +6,16 @@ import io.kontur.eventapi.job.AbstractJob;
 import io.kontur.eventapi.nifc.client.NifcClient;
 import io.kontur.eventapi.nifc.converter.NifcDataLakeConverter;
 import io.micrometer.core.instrument.MeterRegistry;
+import liquibase.repackaged.org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
+import reactor.util.function.Tuple2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.wololo.geojson.Feature;
 import org.wololo.geojson.FeatureCollection;
 import org.wololo.geojson.GeoJSONFactory;
+import reactor.util.function.Tuples;
 
 import java.time.OffsetDateTime;
 import java.util.*;
@@ -23,8 +26,6 @@ import static io.kontur.eventapi.nifc.converter.NifcDataLakeConverter.NIFC_PERIM
 import static io.kontur.eventapi.util.DateTimeUtil.getDateTimeFromMilli;
 import static io.kontur.eventapi.util.JsonUtil.writeJson;
 import static java.time.temporal.ChronoUnit.SECONDS;
-
-import javax.validation.constraints.NotNull;
 
 @Component
 public class NifcImportJob extends AbstractJob {
@@ -69,34 +70,37 @@ public class NifcImportJob extends AbstractJob {
     private void processFeatureCollection(String geoJson, String provider, String updatedAtProp, String externalIdProp) {
         try {
             FeatureCollection fc = (FeatureCollection) GeoJSONFactory.create(geoJson);
-            List<DataLake> dataLakes = new ArrayList<>();
+            Map<Tuple2<String, OffsetDateTime>, DataLake> dataLakes = new HashMap<>();
             Set<String> ids = Arrays.stream(fc.getFeatures())
                     .map(feature -> String.valueOf(feature.getProperties().get(externalIdProp)))
                     .filter(StringUtils::isNotBlank)
                     .collect(Collectors.toSet());
-            Map<String, DataLake> existsDataLakes = new HashMap<>();
+            Map<Tuple2<String, OffsetDateTime>, DataLake> existsDataLakes = new HashMap<>();
             dataLakeDao.getDataLakesByExternalIdsAndProvider(ids, provider)
                     .stream().filter(item -> StringUtils.isNotBlank(item.getExternalId()))
-                    .forEach(dataLake -> existsDataLakes.put(dataLake.getExternalId(), dataLake));
+                    .forEach(dataLake -> existsDataLakes.put(
+                            Tuples.of(dataLake.getExternalId(), dataLake.getUpdatedAt()), dataLake));
             for (Feature feature : fc.getFeatures()) {
                 try {
                     String data = writeJson(feature);
                     String externalId = String.valueOf(feature.getProperties().get(externalIdProp));
                     long updatedAtMilli = Long.parseLong(String.valueOf(feature.getProperties().get(updatedAtProp)));
                     OffsetDateTime updatedAt = getDateTimeFromMilli(updatedAtMilli).truncatedTo(SECONDS);
-                    if (!existsDataLakes.containsKey(externalId)
-                            || !existsDataLakes.get(externalId).getUpdatedAt().isEqual(updatedAt)) {
-                        dataLakes.add(dataLakeConverter.convertDataLake(externalId, updatedAt, provider, data));
+                    if (!existsDataLakes.containsKey(Tuples.of(externalId, updatedAt))
+                            && !dataLakes.containsKey(Tuples.of(externalId, updatedAt))) {
+                        dataLakes.put(Tuples.of(externalId, updatedAt),
+                                dataLakeConverter.convertDataLake(externalId, updatedAt, provider, data));
                     }
                 } catch (Exception e) {
                     LOG.error("Failed to process feature from " + provider, e);
                 }
             }
-            dataLakeDao.storeDataLakes(dataLakes);
+            if (MapUtils.isNotEmpty(dataLakes)) {
+                dataLakeDao.storeDataLakes(dataLakes.values().stream().toList());
+            }
         } catch (Exception e) {
             LOG.error("Failed to process feature collection from " + provider, e);
         }
-
     }
 
     @Override
