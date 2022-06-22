480733f1b1816da5b39a8ab7a0a6b778895e1024
kshmidman <kshmidman@gmail.com>
2022-06-22 15:56:23 +0300

    #10819. Fixed wrong provider name for observations from NHC
    Former-commit-id: a5b29fce3ee7e68f5d9363ee66e04caaf9736298

--- src/main/java/io/kontur/eventapi/nhc/NhcUtil.java
+++ src/main/java/io/kontur/eventapi/nhc/NhcUtil.java
@@ -5,8 +5,8 @@ import java.util.List;
 public final class NhcUtil {
 
     public final static String NHC_AT_PROVIDER = "cyclones.nhc-at.noaa";
-    public final static String NHC_CP_PROVIDER = "cyclones.nhc-ep.noaa";
-    public final static String NHC_EP_PROVIDER = "cyclones.nhc-cp.noaa";
+    public final static String NHC_CP_PROVIDER = "cyclones.nhc-cp.noaa";
+    public final static String NHC_EP_PROVIDER = "cyclones.nhc-ep.noaa";
 
     public final static List<String> NHC_PROVIDERS = List.of(NHC_AT_PROVIDER, NHC_CP_PROVIDER, NHC_EP_PROVIDER);
 }
