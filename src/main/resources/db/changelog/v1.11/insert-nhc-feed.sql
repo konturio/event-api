d5de2fdf408a8377088f1e54fa3865d1e97700f4
kshmidman <kshmidman@gmail.com>
2022-08-03 17:44:25 +0300

    #11508. function st_force3dm changed to another approach for NHC polygons
    Former-commit-id: 69514d0cd9e36408f8c3cb8637136c099274afec

--- src/main/resources/db/changelog/v1.11/insert-nhc-feed.sql
+++ src/main/resources/db/changelog/v1.11/insert-nhc-feed.sql
@@ -3,5 +3,5 @@
 --changeset event-api-migrations:v1.11/insert-nhc-feed.sql runOnChange:false
 
 INSERT INTO feeds (feed_id, alias, description, providers)
-VALUES (uuid_generate_v4(), 'test-nhc', 'Real-time cyclones from NHC',
+VALUES (uuid_generate_v4(), 'test-cyclone', 'Real-time cyclones from NHC',
         '{"cyclones.nhc-at.noaa", "cyclones.nhc-ep.noaa", "cyclones.nhc-cp.noaa"}');
