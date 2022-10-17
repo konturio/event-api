3d3f0c7df0b2103a8f59c239726d641c10254fc4
Polina Krukovich <pkrukovich@kontur.io>
2022-10-17 16:24:34 +0200

    #12971 drop data from feeds table

--- src/main/resources/db/changelog/v1.14/delete-feeds.sql
+++ src/main/resources/db/changelog/v1.14/delete-feeds.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset event-api-migrations:v1.13/delete-feeds.sql runOnChange:false
+--changeset event-api-migrations:v1.13/delete-feeds.sql runOnChange:true
 
 delete from feed_data
 where feed_id != (select feed_id from feeds where alias = 'kontur-public');
@@ -12,4 +12,10 @@ delete from kontur_events
 where provider not in ('firms.modis-c6', 'firms.suomi-npp-viirs-c2', 'firms.noaa-20-viirs-c2', 'kontur.events', 'gdacsAlert', 'gdacsAlertGeometry');
 
 delete from normalized_observations
+where provider not in ('firms.modis-c6', 'firms.suomi-npp-viirs-c2', 'firms.noaa-20-viirs-c2', 'kontur.events', 'gdacsAlert', 'gdacsAlertGeometry');
+
+delete from feeds where alias != 'kontur-public';
+
+update data_lake
+set normalized = false
 where provider not in ('firms.modis-c6', 'firms.suomi-npp-viirs-c2', 'firms.noaa-20-viirs-c2', 'kontur.events', 'gdacsAlert', 'gdacsAlertGeometry');
