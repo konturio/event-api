caa88ad2985ab5174a8db0e169fa896dc0ae80d8
polina-krukovich <polina.krukovich7@gmail.com>
2023-01-25 01:54:52 +0100

    #14312 recalculate events

---
+++ src/main/resources/db/changelog/v1.15/recalculate-events.sql
@@ -0,0 +1,27 @@
+--liquibase formatted sql
+
+--changeset event-api-migrations:v1.15/recalculate-events.sql runOnChange:true
+
+update feed_event_status set actual = false where true;
+
+with events as (
+    select
+        feed_id,
+        event_id,
+        version as "version",
+        (
+            select episode ->> 'type'
+            from jsonb_array_elements(episodes) episode
+            order by (episode ->> 'updatedAt')::timestamptz desc
+            limit 1
+        ) as latest_type
+    from feed_data
+    where type is null
+)
+update feed_data fd
+set
+    type = e.latest_type
+from events e
+where fd.feed_id = e.feed_id
+  and fd.event_id = e.event_id
+  and fd.version = e.version;
