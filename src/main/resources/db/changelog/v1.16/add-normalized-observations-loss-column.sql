15b830aede5ad03a6ea0386621345e18b1545e30
polina-krukovich <polina.krukovich7@gmail.com>
2023-02-01 14:39:54 +0100

    #1290 add jsonb type cast

--- src/main/resources/db/changelog/v1.16/add-normalized-observations-loss-column.sql
+++ src/main/resources/db/changelog/v1.16/add-normalized-observations-loss-column.sql
@@ -3,4 +3,4 @@
 --changeset event-api-migrations:v1.16/add-normalized-observations-loss-column.sql runOnChange:true
 
 alter table normalized_observations
-add column loss jsonb default '{}';
+add column loss jsonb default '{}'::jsonb;
