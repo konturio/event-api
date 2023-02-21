d9355eba12bb8af217fc3ae3114ce6d64edf3a32
kshmidman <kshmidman@gmail.com>
2023-02-21 18:21:08 +0300

    #14963. test

--- src/main/java/io/kontur/eventapi/entity/PdcMapSrvSearchJobs.java
+++ src/main/java/io/kontur/eventapi/entity/PdcMapSrvSearchJobs.java
@@ -15,7 +15,7 @@ public class PdcMapSrvSearchJobs {
     public static final String[] PDC_MAP_SRV_IDS
             = new String[] {"0", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",
             "13", "14", "15", "16", "17", "25",
-            "32", "33", "34", "35", "36", "37", "38", "39"};
+            "32", "33", "34", "35", "36", "37", "38", "39", "42"};
 
     private final List<PdcMapSrvSearchJob> jobs;
 
