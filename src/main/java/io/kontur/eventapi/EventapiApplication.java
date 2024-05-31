a313ba96cb3b9293408d05c7c16919de5c56783c
Aliaksandr Kolesen <aliaksandr.kolesen@gmail.com>
2024-05-31 20:44:08 +0200

    check cache

--- src/main/java/io/kontur/eventapi/EventapiApplication.java
+++ src/main/java/io/kontur/eventapi/EventapiApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class EventapiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EventapiApplication.class, args);
 	}
 
