ced2ddbb265fc513007453efafa0260b20cc8ceb
Andrew <33183728+frolui@users.noreply.github.com>
2025-07-09 16:03:01 +0200

    fix(usgs): use hourly earthquake feed

--- src/main/java/io/kontur/eventapi/usgs/earthquake/client/UsgsEarthquakeClient.java
+++ src/main/java/io/kontur/eventapi/usgs/earthquake/client/UsgsEarthquakeClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(value = "usgsEarthquakeClient", url = "${usgs.host}")
 public interface UsgsEarthquakeClient {
 
-    @GetMapping("/earthquakes/feed/v1.0/summary/all_day.geojson")
+    @GetMapping("/earthquakes/feed/v1.0/summary/all_hour.geojson")
     String getEarthquakes();
 
     @GetMapping("/earthquakes/feed/v1.0/detail/{id}.geojson")
