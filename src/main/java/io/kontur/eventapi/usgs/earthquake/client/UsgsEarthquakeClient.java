933429ac3550be141a0566404114c509cf98e79e
Andrew <33183728+frolui@users.noreply.github.com>
2025-07-09 15:19:31 +0200

    fix(usgs): switch to daily earthquake feed

--- src/main/java/io/kontur/eventapi/usgs/earthquake/client/UsgsEarthquakeClient.java
+++ src/main/java/io/kontur/eventapi/usgs/earthquake/client/UsgsEarthquakeClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(value = "usgsEarthquakeClient", url = "${usgs.host}")
 public interface UsgsEarthquakeClient {
 
-    @GetMapping("/earthquakes/feed/v1.0/summary/all_hour.geojson")
+    @GetMapping("/earthquakes/feed/v1.0/summary/all_day.geojson")
     String getEarthquakes();
 
     @GetMapping("/earthquakes/feed/v1.0/detail/{id}.geojson")
