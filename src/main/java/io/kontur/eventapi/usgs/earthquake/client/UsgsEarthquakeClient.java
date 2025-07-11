52d6e08661d6e11459f4656e5b2030ad9eff0cb0
Andrew <33183728+frolui@users.noreply.github.com>
2025-07-11 17:36:52 +0200

    fix(usgs): restore 4.5-week feed

--- src/main/java/io/kontur/eventapi/usgs/earthquake/client/UsgsEarthquakeClient.java
+++ src/main/java/io/kontur/eventapi/usgs/earthquake/client/UsgsEarthquakeClient.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @FeignClient(value = "usgsEarthquakeClient", url = "${usgs.host}")
 public interface UsgsEarthquakeClient {
 
-    @GetMapping("/earthquakes/feed/v1.0/summary/all_hour.geojson")
+    @GetMapping("/earthquakes/feed/v1.0/summary/4.5_week.geojson")
     String getEarthquakes();
 }
