96fa00c753da239661c809dec7fac943bd552285
Andrew <33183728+frolui@users.noreply.github.com>
2025-07-16 13:34:55 +0200

    fix(db): keep kontur_public unchanged

--- src/main/resources/db/changelog/v1.24.0/add-usgs-to-feeds.sql
+++ src/main/resources/db/changelog/v1.24.0/add-usgs-to-feeds.sql
@@ -10,7 +10,3 @@ update feeds
 set providers = '{"wildfire.calfire","wildfire.perimeters.nifc","wildfire.locations.nifc","storms.noaa","gdacsAlert","gdacsAlertGeometry","cyclones.nhc-at.noaa","cyclones.nhc-ep.noaa","cyclones.nhc-cp.noaa","usgs.earthquake"}'::text[],
     description = 'Feed from California Department of Forestry and Fire Protection (CAL FIRE), National Interagency Fire Center (NIFC), National Oceanic and Atmospheric Administration (NOAA, Storm Events Database), Global Disaster Alert and coordination system (GDACS), National Hurricane Center (NHC), US Geological Survey (USGS)'
 where feed_id = 'f26f09c9-f5e6-43f8-8c92-34bb8739b106';
-
-update feeds
-set providers = '{"gdacsAlert","gdacsAlertGeometry","kontur.events","usgs.earthquake"}'::text[]
-where alias = 'kontur-public';
