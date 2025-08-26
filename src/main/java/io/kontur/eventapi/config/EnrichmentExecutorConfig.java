03c41563bdc07150eb0535e0ab582602b68ee791
sleeping <hh.sleeping@gmail.com>
2025-08-26 12:29:31 +0400

    add setting for enrichment threads

--- src/main/java/io/kontur/eventapi/config/EnrichmentExecutorConfig.java
+++ src/main/java/io/kontur/eventapi/config/EnrichmentExecutorConfig.java
@@ -1,5 +1,6 @@
 package io.kontur.eventapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,14 +11,19 @@ import java.util.concurrent.Executor;
 @Configuration
 @EnableAsync(proxyTargetClass = true)
 public class EnrichmentExecutorConfig {
+
+    @Value("${enrichmentExecutor.poolSize:5}")
+    private int enrichmentExecutorPoolSize;
+
     /**
      * This bean configures executor for {@link io.kontur.eventapi.enrichment.EventEnrichmentTask}.
+     * Thread count is configurable with {@code enrichmentExecutor.poolSize} property.
      */
     @Bean(name = "enrichmentExecutor")
     public Executor getAsyncExecutor() {
         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(5);
+        executor.setCorePoolSize(enrichmentExecutorPoolSize);
+        executor.setMaxPoolSize(enrichmentExecutorPoolSize);
         executor.setQueueCapacity(10000);
         executor.setThreadNamePrefix("EnrichmentThread-");
         executor.initialize();
